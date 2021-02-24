767ce96cdba95db9c9ba05d74e12c08f8ed323e6
sreekar2307 <bollamsreekhar@gmail.com>
2021-02-24 12:59:05 +0530

    added a example on how to use scaffold.js
    Took an example on how Github api manages resources

---
+++ examples/scaffold-git.js
@@ -0,0 +1,46 @@
+const scaffold = require('..');
+const faker = require('faker');
+const schema = {
+  types: {
+    Commit: {
+      author: 'User',
+      commitTime: 'date',
+      committer: 'User',
+      message: 'text',
+      sha: {
+        _func: [faker.git.commitSha],
+      },
+    },
+    Repository: {
+      _hasMany: 'Commit',
+      description: 'text',
+      forks: 'number',
+      id: 'id',
+      name: 'string',
+      owner: 'User',
+      private: 'boolean',
+      url: {
+        _func: [faker.internet.url],
+      },
+    },
+    User: {
+      _hasMany: 'Repository',
+      company: 'string',
+      email: {
+        _func: [faker.internet.email, 'scaffold', 'awesome'],
+      },
+      id: 'id',
+      name: 'string',
+    },
+  },
+};
+const { userGenerator } = scaffold({
+  schema,
+});
+const user = userGenerator();
+const userRepos = user.repositories();
+const firstRepoCommits = userRepos[0].commits();
+
+console.log(user);
+console.log(userRepos);
+console.log(firstRepoCommits);
